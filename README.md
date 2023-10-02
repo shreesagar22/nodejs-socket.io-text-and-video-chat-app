f87ab5fac305956942bc891d5791ebab1df5eead
Shree Sagar <imtheshadows24@gmail.com>
2023-10-02 10:49:29 +0530

    added server code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # video-call-chat-app
+
 A video call application which support group video call as well as chatting
---
+++ server/middleware/authSocket.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const config = process.env;
+
+const verifyTokenSocket = (socket, next) => {
+
+  console.log('inside veify', socket)
+
+  const token = socket.handshake.auth?.token;
+
+  try {
+    const decoded = jwt.verify(token, config.TOKEN_KEY);
+    socket.user = decoded;
+  } catch (err) {
+    const socketError = new Error("NOT_AUTHORIZED");
+    return next(socketError);
+  }
+
+  next();
+};
+
+module.exports = verifyTokenSocket;
